e79e8642fd2182ec76c31f0c40c2449a851ad444
stylop <stylop>
2022-11-02 10:24:44 +0100

    -

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['https://web-production-2bf5.up.railway.app', '127.0.0.1']
+# ALLOWED_HOSTS = ['https://web-production-2bf5.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
